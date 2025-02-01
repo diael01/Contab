508541eeeb8af72df900bb9ef006f9796a9de307
Elena D Diaconu <diael01@live.com>
2025-02-01 10:45:57 +0200

    Generate DB Scripts

--- Database/SQL/Helpers.sql
+++
@@ -1,7 +0,0 @@
-/****** Script for SelectTopNRows command from SSMS  ******/
-SELECT * FROM [Contab].[dbo].[Organisation]
-SELECT * FROM [Contab].[dbo].[Employee]
-
-delete FROM [Contab].[dbo].[Employee]
-delete FROM [Contab].[dbo].[Organisation]
-  
