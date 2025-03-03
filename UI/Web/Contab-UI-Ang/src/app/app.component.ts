c23bdf062113d2fe741b5d2080010c91118dd5e1
diael01 <diael01@live.com>
2025-03-03 21:22:15 +0200

    Add Home, Org and Site-Header comps

--- UI/Web/Contab-UI-Ang/src/app/app.component.ts
+++ UI/Web/Contab-UI-Ang/src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { SiteHeaderComponent } from './site-header/site-header.component';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  //imports: [SiteHeaderComponent],
 })
 export class AppComponent {
   title = 'Contab-UI-Ang';
--- UI/Web/Contab-UI-Ang/src/app/app.module.ts
+++ UI/Web/Contab-UI-Ang/src/app/app.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { OrgComponent } from './org/org.component';
+import { EmpComponent } from './emp/emp.component';
+import {SiteHeaderComponent} from './site-header/site-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    OrgComponent,
+    EmpComponent,
+    SiteHeaderComponent,
+
   ],
   imports: [
     BrowserModule
