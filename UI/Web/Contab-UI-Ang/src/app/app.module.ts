439ec93f70c9d4621732c472b0ca20e2349766c9
diael01 <diael01@live.com>
2025-03-04 17:12:18 +0200

    Add Org Dynamic Tree, not form API yet

--- UI/Web/Contab-UI-Ang/src/app/app.module.ts
+++ UI/Web/Contab-UI-Ang/src/app/app.module.ts
@@ -4,20 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { OrgComponent } from './org/org.component';
-import { EmpComponent } from './emp/emp.component';
 import {SiteHeaderComponent} from './site-header/site-header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTreeModule } from '@angular/material/tree';
+import { MatIconModule } from '@angular/material/icon';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     OrgComponent,
-    EmpComponent,
-    SiteHeaderComponent,
-
+    SiteHeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatTreeModule,
+    MatIconModule,
+    MatProgressBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
