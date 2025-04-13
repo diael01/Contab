36f58f9160e16cd751e45dba2db9378f2bf11665
diael01 <diael01@live.com>
2025-04-13 21:29:01 +0300

    a. Resolve package deps, b. Add few components

--- UI/Web/ReacTS/src/App.tsx
+++ UI/Web/ReacTS/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+/* import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -23,4 +23,39 @@ function App() {
   );
 }
 
+export default App; */
+import React from 'react';
+import Footer from './components/Footer';
+/* import { useRoutes } from 'react-router-dom';
+import router from 'src/router';
+
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+
+import { CssBaseline } from '@mui/material';
+import ThemeProvider from './theme/ThemeProvider'; */
+
+function App() {
+  //const content = useRoutes(router);
+
+  return (
+    <div className="App">
+    <header className="App-header">
+    
+      <p>
+        Edit <code>src/App.tsx</code> and save to reload.
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </header>
+    <Footer/>
+  </div>
+  );
+}
 export default App;
